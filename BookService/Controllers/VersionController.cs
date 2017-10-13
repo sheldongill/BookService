b17b7f385165603a3d32010cd0e65e18ea32d330
Sheldon Gill <sheldon.gill@health.wa.gov.au>
2017-10-13 09:24:54 +0800

    Add version endpoint to service

---
+++ BookService/Controllers/VersionController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace ServiceShell.Controllers
+{
+    /// <summary>
+    ///  returns current build version of service
+    /// </summary>
+    [Route("/[controller]")]
+    public class VersionController : Controller
+    {
+        public VersionController()
+        {
+        }
+
+        /// <summary>
+        /// Retrieve the version information: Major, Minor, Patch and additional for build or whatever
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var runtimeVersion = typeof(Startup).GetTypeInfo().Assembly.GetCustomAttribute<AssemblyFileVersionAttribute>();
+            var tokens = runtimeVersion.Version.Split('.');
+            return tokens;
+        }
+    }
+}
